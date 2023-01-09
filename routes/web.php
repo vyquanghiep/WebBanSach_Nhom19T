45fcac6e0c315c9b93537159bc1de39754dba103
toan2310 <nguyenquoctoan0709@gmail.com>
2023-01-09 18:18:03 +0700

    Upload chuc nang gio hang va tim kiem

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,16 @@ Route::post('/save_category', 'CategoryProduct@add_category');
 Route::get('/delete_category/{categoryid}', 'CategoryProduct@delete_category');
 Route::get('/update_category/{categoryid}', 'CategoryProduct@show_form_edit_category');
 Route::post('/save_editcategory', 'CategoryProduct@update_category');
+//cart
+Route::post('/save-cart', 'CartController@save_cart');
+Route::get('/show-cart', 'CartController@show_cart');
+Route::get('/delete-to-cart/{rowId}', 'CartController@delete_to_cart');
+Route::post('/update-cart-quantity', 'CartController@update_cart_quantity');
+Route::post('/login-cart', 'CartController@login_cart');
+Route::post('/signin-cart', 'CartController@signin_cart');
+
+//checkout
+Route::post('/payment', 'CheckoutController@payment');
+// search
+Route:: get('/search', 'SearchController@show_search_results');
+Route:: post('/get_suggestion', 'SearchController@show_suggestion');
